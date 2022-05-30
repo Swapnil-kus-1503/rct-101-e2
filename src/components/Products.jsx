4f404a5c19d54d108f60d6ec2957d7c943426047
Swapnil-kus-1503 <ashukushwaha15031997@gmail.com>
2022-05-30 15:56:53 +0530

    evalution2

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,32 @@
 import React from "react";
-
+import AddProduct from "./AddProduct";
+import Pagination from "./Pagination";
+import Product from "./Product";
+import { Flex, Grid } from "@chakra-ui/react";
+import axios from "axios";
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+
+  const [product, setProduct] = React.useState([])
+
+  React.useEffect(() => {
+    const getProduct = async () => {
+      let res = await axios.get(`http://localhost:8080/products`);
+      setProduct(res.data);
+      console.log(res.data);
+    };
+    getProduct();
+  },[]);
 
   return (
     <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
-      {/* Pagination */}
+      <AddProduct />
+      <Grid>
+        <Product product={product} />
+      </Grid>
+      <Pagination />
     </Flex>
   );
 };
